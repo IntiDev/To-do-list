dca90fc2c137dfd57bc1f5a80a9dbfcfd3b41466
Inti <yeseniase@outlook.it>
2019-06-23 22:07:10 -0500

    Se agrega estructura básica de proyecto

---
+++ main.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: 'main',
+    data: {
+        txtItem: 'Lorem ipsum',
+        itemList: []
+    },
+
+    methods:{
+        addItem: () => {
+            let item = this.txtItem.trim();
+            if(item) {
+                this.itemList.push({
+                        text: item,
+                        checked: false
+                });
+            }
+            this.txtItem = '';
+            console.log("Agregada");
+            
+        }
+    }
+});
